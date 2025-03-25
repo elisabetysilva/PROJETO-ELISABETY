682b2a337f819079130b5a11409dd0fa84946b68
elisabetysilva <164500441+elisabetysilva@users.noreply.github.com>
2025-03-25 07:59:04 -0300

    .

--- script.js
+++ script.js
@@ -16,3 +16,5 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
